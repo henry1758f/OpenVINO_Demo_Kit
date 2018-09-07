9d1cf590b7cf3916893c068fd81e7ed747414504
henry1758f <henry1758f@gmail.com>
2018-09-08 02:27:01 +0800

    Init OpenVINO_demo_SYNNEX.sh

---
+++ OpenVINO_demo_SYNNEX.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# File: OpenVINO_demo_SYNNEX.sh
+# ver 0.0.1
+# This is design for connecting to the share folder on TWNB17034
+# 2018/09/07 	henry1758	0.0.1 	first-create
+#
+
+export VERSION="0.0.1"
+
+echo
+echo "|=========================================|"
+echo "|  SYNNEX TECHNOLOGY INTERNATIONAL CORP.  |"
+echo "|                                         |"
+echo "|        Intel OpenVINO Demostration      |"
+echo "|=========================================|"
+echo "          Version $VERSION                 "
+echo ""
+echo ""
